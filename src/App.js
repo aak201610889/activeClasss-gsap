529d8d966d1aa954e67b4caa457e46f2d85b1d5d
ayham-kattan <aak201610885@gmail.com>
2025-05-14 05:05:55 -0700

    activeButton

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import Slider from "./components/Slider";
+import ActiveButton from "./components/ActiveButton";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Slider />} />
+          <Route path="/activeButton" element={<ActiveButton />} />
         </Routes>
       </div>
     </Router>
